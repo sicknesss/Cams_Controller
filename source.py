22c0ce1c56a3ac265f3a3938441999e1e03bdd79
GorbatovDS <50554014+GorbatovDS@users.noreply.github.com>
2019-10-21 20:30:18 +0300

    Add files via upload

---
+++ source.py
@@ -0,0 +1,21 @@
+# from typing import re
+
+import rtsp as r
+import time
+import requests as rq
+
+print('Input web controller IP\n')
+web_address = input()
+print('Input presentation shortcut number\n')
+Input_number = input()
+presentation_address = 'http://' + web_address + '/api/?Function=Cut&Input=' + Input_number
+return_to_address = 'http://' + web_address + '/api/?Function=Cut&Input=0'
+
+while 1:
+    case = int(input())
+    if case == 1:
+        print(rq.get(presentation_address))
+        time.sleep(5)
+        print(rq.get(return_to_address))
+    else:
+        break
